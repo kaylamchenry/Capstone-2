1aada86305bbcc3a3d0635562ae0c82052b2d82c
kaylamchenry <kayla.mchenry@gmail.com>
2023-11-21 11:08:21 -0800

    firstsavepoint

---
+++ example.js
@@ -0,0 +1,46 @@
+"use strict";
+function cookingEggs(numberofEggs) {
+//input
+let numberofEggs = numberofEggs
+
+//algorithm
+console.log("turn on the stove");
+console.log("wait until water boils");
+console.log("put the" + numberofEggs+ 'eggs into the water");:
+console.log("wait 6 mminutes");
+console.log("take eggs out of the water");
+console.log("cool them down in coold water");
+
+//output
+console.log("serve 6 eggs to 3 people");
+
+
+}
+
+function displayfullname(firstname,lastname){
+//input
+let firstname = "Kayla";
+let lastname = "McHenry";
+
+//algorthim
+let fullname = `Hi my name is ${firstname} ${lastname}`
+
+//output: "Hi my name is Firstname,Lastname"
+console.log('Hi my name is Firstname,Lastname')
+
+}
+
+//DESCRIPTION:
+//Your Job
+//Find the sum of all multiples of n below m
+
+//Keep in Mind
+//n and m are natural numbers (positive integers)
+//m is excluded from the multiples
+//Examples
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID"
+
+
